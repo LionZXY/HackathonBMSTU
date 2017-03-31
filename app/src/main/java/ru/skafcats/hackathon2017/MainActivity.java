000051a9745b1259206f0cfbed4eeb2efd738977
Vasiliy <devasidmi@gmail.com>
2017-03-31 16:14:33 +0300

    Big commit #2
    -- Refresh grade settings
    -- Added needed libraries

--- app/src/main/java/ru/skafcats/hackathon2017/MainActivity.java
+++ app/src/main/java/ru/skafcats/hackathon2017/MainActivity.java
@@ -2,12 +2,18 @@ package ru.skafcats.hackathon2017;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
     }
 }
---
+++ app/src/main/java/ru/skafcats/hackathon2017/fragments/PasswordListFragment.java
@@ -0,0 +1,21 @@
+package ru.skafcats.hackathon2017.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vasidmi on 31/03/2017.
+ */
+
+public class PasswordListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.la)
+    }
+}
