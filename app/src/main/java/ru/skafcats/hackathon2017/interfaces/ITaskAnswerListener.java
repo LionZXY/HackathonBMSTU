83690ded49c1d0ec45ae50d02928322ac95fd724
lionzxy <nikita@kulikof.ru>
2017-03-31 13:35:30 +0300

    Добавил асинхронную работу с сервисами

---
+++ app/src/main/java/ru/skafcats/hackathon2017/interfaces/ITaskAnswerListener.java
@@ -0,0 +1,11 @@
+package ru.skafcats.hackathon2017.interfaces;
+
+import android.os.Bundle;
+
+/**
+ * Created by lionzxy on 26.03.17.
+ */
+
+public interface ITaskAnswerListener {
+    public void onAnswer(Bundle data);
+}
