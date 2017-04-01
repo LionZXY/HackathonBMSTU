2b10239bb187ad406ff6905913de024d07fbc2e2
lionzxy <nikita@kulikof.ru>
2017-04-01 10:54:04 +0300

    Фрагменты регистрации, авторизации и пинкода

--- app/src/main/java/ru/skafcats/hackathon2017/MainActivity.java
+++ app/src/main/java/ru/skafcats/hackathon2017/MainActivity.java
@@ -1,71 +1,21 @@
 package ru.skafcats.hackathon2017;
 
-import android.content.Intent;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 
-import ru.skafcats.crypto.CryptoApi;
-import ru.skafcats.crypto.enums.Constants;
-import ru.skafcats.crypto.interfaces.ITaskAnswerListener;
-import ru.skafcats.crypto.services.MultiResultReciever;
-import ru.skafcats.hackathon2017.face_unlock.FaceUnlockActivity;
-import ru.skafcats.hackathon2017.pin_unlock.PinActivity;
+import ru.skafcats.hackathon2017.fragments.PinFragment;
 
-public class MainActivity extends AppCompatActivity implements ITaskAnswerListener{
-    String password = "1234";
-    Toolbar mToolbar;
-    FragmentManager mFragmentManager;
-
-
-    private int pinCode;
-    private int photoCode;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent photoIntent = new Intent(this, FaceUnlockActivity.class);
-        startActivityForResult(photoIntent, 2);
-
-        Intent pinIntent = new Intent(this, PinActivity.class);
-        startActivityForResult(pinIntent, 1);
-    }
-
-    @Override
-    public void onAnswer(Bundle data) {
-        int code = data.getInt(Constants.KEY_RESULT_CODE, MultiResultReciever.CODE_RESULT_ERROR_TASK);
-        Log.i("TEST", code + ' ' + data.getString(Constants.KEY_RESPONSE));
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 1:
-                pinCode = data.getIntExtra("code", 0);
-                break;
-
-            case 2:
-                photoCode = data.getIntExtra("code", 0);
-
-//                CryptoApi cryptoApi = CryptoApi.getInstance(this, "" + getCode());
-                // TODO
-                //cryptoApi.register("nikita@kulikof.ru",password, null);
-                //cryptoApi.addInfo(new SecureInfo("test2"));
-//                cryptoApi.login("nikita@kulikof.ru", password, null);
-//                cryptoApi.sync();
-
-                Intent intent = new Intent(this, SecureInfoActivity.class);
-                startActivity(intent);
-
-                break;
+        setContentView(R.layout.activity_main);
+        FragmentManager mFragmentManager = getFragmentManager();
+        if (mFragmentManager.findFragmentById(R.id.main_fragment) == null) {
+            mFragmentManager.beginTransaction().add(R.id.main_fragment, new PinFragment(), PinFragment.TAG).commit();
         }
-    }
 
-    private long getCode() {
-        return (long) photoCode << 16 + pinCode;
     }
 }
